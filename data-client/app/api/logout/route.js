ee5357396d071a7464fd7803441630fad53b6631
dcallemerthin <112483455+dcallemerthin@users.noreply.github.com>
2023-10-24 16:52:01 -0400

    testing branch

--- data-client/app/api/logout/route.js
+++ data-client/app/api/logout/route.js
@@ -1,18 +1,23 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import { getAPIClient } from '@/utils/webflow_helper';
 
-const { CLIENT_ID, CLIENT_SECRET } = process.env;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 export async function POST(request) {
   const cookieStore = cookies();
   if (!cookieStore.has('webflow_auth')) {
-    return NextResponse.json({ok: true});
+    return NextResponse.json({ ok: true });
   }
   const webflowAuth = cookieStore.get('webflow_auth').value;
   const webflowAPI = getAPIClient(webflowAuth, false);
-  const res = await webflowAPI.revokeToken({access_token: webflowAuth, client_id: CLIENT_ID, client_secret: CLIENT_SECRET });  
+  const res = await webflowAPI.revokeToken({
+    access_token: webflowAuth,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+  });
   const response = NextResponse.json(res);
-  response.cookies.delete('webflow_auth')
+  response.cookies.delete('webflow_auth');
   return response;
 }
